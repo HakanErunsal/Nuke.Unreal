8c41fa7333e12d9b29346b08974ae6d86a99d404
HakanErunsal <hakanerunsal2@gmail.com>
2025-11-24 22:31:18 +0000

    Deploying to gh-pages from @ HakanErunsal/Nuke.Unreal@fdfe46ec3a24d37a145fc912c5c08a9b54687b3e 🚀

---
+++ navtreedata.js
@@ -0,0 +1,150 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Nuke.Unreal", "index.html", [
+    [ "Features:", "index.html#autotoc_md8", null ],
+    [ "Legal", "index.html#Legal", null ],
+    [ "Using third-party C++ libraries", "d2/d84/CppLibraries.html", [
+      [ "Use library from XRepo", "d2/d84/CppLibraries.html#autotoc_md0", [
+        [ "Library specifications", "d2/d84/CppLibraries.html#autotoc_md1", null ]
+      ] ],
+      [ "Use library via CMake", "d2/d84/CppLibraries.html#autotoc_md2", null ],
+      [ "Use header only library", "d2/d84/CppLibraries.html#autotoc_md3", null ]
+    ] ],
+    [ "Bitter Recompense", "da/dc1/FAQ.html", [
+      [ "Installation", "da/dc1/FAQ.html#autotoc_md4", null ],
+      [ "Plugins", "da/dc1/FAQ.html#autotoc_md5", null ]
+    ] ],
+    [ "Getting started", "d8/d51/GettingStarted.html", [
+      [ "Install via remote script", "d8/d51/GettingStarted.html#autotoc_md6", null ],
+      [ "Install manually", "d8/d51/GettingStarted.html#autotoc_md7", null ]
+    ] ],
+    [ "Miscellaneous things", "da/d19/Misc.html", [
+      [ "Alternatives", "da/d19/Misc.html#autotoc_md9", null ],
+      [ "Articles", "da/d19/Misc.html#autotoc_md10", null ]
+    ] ],
+    [ "Setting up for plugin development", "d5/d8c/PluginSetup.html", null ],
+    [ "Setting up for a project", "da/d1e/ProjectSetup.html", null ],
+    [ "Semi-auto Runtime Dependencies", "d4/d20/RuntimeDeps.html", null ],
+    [ "Targets", "d3/de9/Targets.html", [
+      [ "Global targets (UnrealBuild)", "d3/de9/Targets.html#autotoc_md11", [
+        [ "info", "d3/de9/Targets.html#target-info", null ],
+        [ "switch", "d3/de9/Targets.html#target-switch", null ],
+        [ "prepare", "d3/de9/Targets.html#target-prepare", null ],
+        [ "generate", "d3/de9/Targets.html#target-generate", null ],
+        [ "build-editor", "d3/de9/Targets.html#target-build-editor", null ],
+        [ "build", "d3/de9/Targets.html#target-build", null ],
+        [ "cook", "d3/de9/Targets.html#target-cook", null ],
+        [ "clean", "d3/de9/Targets.html#target-clean", null ],
+        [ "run", "d3/de9/Targets.html#target-run", null ],
+        [ "run-uat", "d3/de9/Targets.html#target-run-uat", null ],
+        [ "run-ubt", "d3/de9/Targets.html#target-run-ubt", null ],
+        [ "run-shell", "d3/de9/Targets.html#target-run-shell", null ],
+        [ "run-editor-cmd", "d3/de9/Targets.html#target-run-editor-cmd", null ],
+        [ "ensure-build-plugin-support", "d3/de9/Targets.html#target-ensure-build-plugin-support", null ]
+      ] ],
+      [ "UnrealBuild.Templating", "d3/de9/Targets.html#autotoc_md12", null ],
+      [ "Packaging (IPackageTargets)", "d3/de9/Targets.html#targets-IPackageTargets", [
+        [ "package", "d3/de9/Targets.html#target-package", null ]
+      ] ],
+      [ "Targeting Android (IAndroidTargets)", "d3/de9/Targets.html#autotoc_md13", [
+        [ "apply-sdk-user-settings", "d3/de9/Targets.html#target-apply-sdk-user-settings", null ],
+        [ "clean-intermediate-android", "d3/de9/Targets.html#target-clean-intermediate-android", null ],
+        [ "sign-apk", "d3/de9/Targets.html#target-sign-apk", null ],
+        [ "install-on-android", "d3/de9/Targets.html#target-install-on-android", null ],
+        [ "debug-on-android", "d3/de9/Targets.html#target-debug-on-android", null ],
+        [ "java-development-service", "d3/de9/Targets.html#target-java-development-service", null ]
+      ] ]
+    ] ],
+    [ "Unreal boilerplate templates", "d3/d38/Templates.html", [
+      [ "Use your own templates", "d3/d38/Templates.html#autotoc_md14", null ]
+    ] ],
+    [ "Unreal tools bindings", "d2/d94/UnrealTools.html", [
+      [ "Passing command line arguments to Unreal tools", "d2/d94/UnrealTools.html#autotoc_md15", [
+        [ "Custom UBT or UAT arguments", "d2/d94/UnrealTools.html#autotoc_md16", null ]
+      ] ],
+      [ "C# Tool API", "d2/d94/UnrealTools.html#autotoc_md17", [
+        [ "Handling multiple egnine versions", "d2/d94/UnrealTools.html#autotoc_md18", null ]
+      ] ]
+    ] ],
+    [ "Packages", "namespaces.html", [
+      [ "Package List", "namespaces.html", "namespaces_dup" ],
+      [ "Package Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Properties", "functions_prop.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"d0/d77/classNuke_1_1Unreal_1_1Tools_1_1UbtConfigGenerated_1_1InlineGeneratedCppsConfig.html#adaad9d6634e6bb0aad3b312ccf6f15ea",
+"d1/d0f/namespaceNuke_1_1Unreal_1_1Tools.html#a952cb0d59736817894f6f417f0c616f7adf49adab93b9e0c10c64f72458b31971",
+"d1/d93/classNuke_1_1Unreal_1_1Tools_1_1UatConfigGenerated_1_1BuildGraphConfig.html#aad3f922fc801387856584bf8e80eac98",
+"d1/de4/classNuke_1_1Unreal_1_1Tools_1_1UatConfigGenerated_1_1ProjectParamsConfig.html#a9d05410eb9fae27f7a782200d887e94d",
+"d2/d94/UnrealTools.html#autotoc_md17",
+"d3/d98/classNuke_1_1Unreal_1_1Unreal.html#a5f17b7cc195d6d03351b386ed90cc202",
+"d4/db1/classNuke_1_1Unreal_1_1Tools_1_1UatConfigGenerated_1_1ProgramConfig.html#a7552cfe91505d78bb1fb87ddc63cc12c",
+"d5/d9a/classNuke_1_1Unreal_1_1Tools_1_1UbtConfigGenerated_1_1CppCompileWarningsConfig.html#acda9194234186677a68743a722135966",
+"d7/d0d/classNuke_1_1Unreal_1_1Tools_1_1UbtConfigGenerated.html#a0f306d5aaa63fad565b26d401f7c5a70",
+"d7/d0d/classNuke_1_1Unreal_1_1Tools_1_1UbtConfigGenerated.html#a59c2660bc65cb98c356cf435ef9dc8c1",
+"d7/d0d/classNuke_1_1Unreal_1_1Tools_1_1UbtConfigGenerated.html#aa18e651d09817e0825d80d5f26e8bdf7",
+"d7/d0d/classNuke_1_1Unreal_1_1Tools_1_1UbtConfigGenerated.html#aede475e5d3a20d063db5d2780701f6e6",
+"d7/d7b/classNuke_1_1Unreal_1_1Tools_1_1UbtConfigGenerated_1_1UnrealBuildAcceleratorConfigConfig.html#a7420f13398c7ff72586fcefa8f7610cd",
+"d7/dd3/classNuke_1_1Unreal_1_1Tools_1_1UbtConfigGenerated_1_1PVSGatherConfig.html#a7c4ee38920d80d0f7c59b402258762eb",
+"d8/d1a/classNuke_1_1Unreal_1_1Tools_1_1UatConfigGenerated.html#a61a8005fdf147159391cf72e5b26d2e6",
+"d8/d1a/classNuke_1_1Unreal_1_1Tools_1_1UatConfigGenerated.html#ac587e6bebc30df1fe216b234c04aefd5",
+"d8/d36/classNuke_1_1Unreal_1_1Tools_1_1UatConfigGenerated_1_1BuildCookRunConfig.html#a364418482d8eecfe72b03bee487997ff",
+"d8/d36/classNuke_1_1Unreal_1_1Tools_1_1UatConfigGenerated_1_1BuildCookRunConfig.html#af2670ab37cad9aa952bcb4da28628719",
+"d9/d80/classNuke_1_1Unreal_1_1Tools_1_1UatConfigGenerated_1_1NinjaBuildTargetPlatform__MacConfig.html#a9d140414a71ba0823babef46ca4ac425",
+"da/d40/classNuke_1_1Unreal_1_1Tools_1_1UbtConfigGenerated_1_1BuildConfig.html#a9abd964ac8b882021728ac04dc0a0113",
+"da/dc4/classNuke_1_1Unreal_1_1Tools_1_1UbtConfigGenerated_1_1TargetRulesConfig.html#a53f0f83ac950d25390a1442ec22ec8de",
+"db/d40/classNuke_1_1Unreal_1_1Tools_1_1ToolConfig.html#a7fc33733ec364a22205200be8cc560d5",
+"dc/dae/classNuke_1_1Unreal_1_1Tools_1_1UatConfigGenerated_1_1BenchmarkBuildConfig.html#a3971ccaf965746402d6bef3e86a714d2",
+"dd/d86/classNuke_1_1Unreal_1_1Tools_1_1UatConfigGenerated_1_1CreatePlatformExtensionConfig.html#a401f495e05f8fe0198dd16b08e35ace4",
+"de/ddd/classNuke_1_1Unreal_1_1Tools_1_1UbtConfigGenerated_1_1VSCodeProjectFileGeneratorConfig.html#a8d9c6a318092fe52d17fc6a41d7660b2",
+"df/df3/classNuke_1_1Unreal_1_1Tools_1_1UatConfigGenerated_1_1NinjaBuildTargetPlatform__TVOSConfig.html#ab65da5ddce02be1877763d0bf16cbb50"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
